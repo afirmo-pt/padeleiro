c727146dcc75763600d091cbd885bc76c6ea969e
afirmo-pt <afirmo@gmail.com>
2026-07-22 13:11:08 +0100

    fix: adicionar import AppRoutes no match_detail_screen.dart

--- padeleiro_app/lib/features/match/presentation/match_detail_screen.dart
+++ padeleiro_app/lib/features/match/presentation/match_detail_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../core/router/app_router.dart';
 import '../../../core/theme/app_theme.dart';
 import '../../auth/data/auth_providers.dart';
 import '../data/match_providers.dart';
